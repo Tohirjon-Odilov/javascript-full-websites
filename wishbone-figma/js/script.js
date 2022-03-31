278176803fa393155f1bec367a0ffb2663725fc4
Shahanshoh03 <82634626+Tohirjon-Odilov@users.noreply.github.com>
2022-03-31 16:55:11 +0300

    delete some file

--- wishbone-figma/js/script.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLikns = document.querySelector('.navbar_menu')
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLikns.classList.toggle('active');
-})
